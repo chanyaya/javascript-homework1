2e8572135ec1f575a5ebe87ec4d261900a75a204
chanyaya <a.chanyanit@gmail.com>
2022-03-23 19:29:36 +0700

    edited 02-05

--- 02-Variable/05/main.js
+++ 02-Variable/05/main.js
@@ -1,6 +1,6 @@
 let BRAND_NAME = 'I am a hero'
 
-- ประกาศตัวแปรชื่อ BRAND_NAME ให้ค่าเป็น ‘I am a hero’
-- สามารถประกาศชื่อตัวแปรชื่อตัวพิมพ์ใหญ่ทั้งหมดได้หรือไม่ ถ้าได้นิยมใช้ในกรณีอะไร
+// - ประกาศตัวแปรชื่อ BRAND_NAME ให้ค่าเป็น ‘I am a hero’
+// - สามารถประกาศชื่อตัวแปรชื่อตัวพิมพ์ใหญ่ทั้งหมดได้หรือไม่ ถ้าได้นิยมใช้ในกรณีอะไร
 
 // All-capitalized variable helps with clarities. It is, therefore, often used to declare value that are likely to remain constant. This is not Javascript's rule but a tradition thingy. 
